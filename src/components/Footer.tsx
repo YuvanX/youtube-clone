95f3a5ea02bf75a9d55ba169a5592bfe833ca518
YuvanX <abhivigneshofficial@gmail.com>
2024-12-30 14:02:54 +0530

    footertextfixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { RiArrowRightUpLine } from "@remixicon/react";
 
 const Footer = () => {
   return (
-    <div className="flex justify-between w-full text-white font-space items-center py-8 ">
+    <div className="flex flex-col justify-start md:flex-row md:justify-between w-full text-white font-space items-start md:items-center py-8 ">
       <div className="flex items-center gap-2 font-light cursor-pointer hover:underline hover:text-gray-300">
         <div>
           <RiArrowRightUpLine size={40} />
